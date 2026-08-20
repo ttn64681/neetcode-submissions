28faa6d698499062f54cf07a370b81eba91b393f
Thai Tam-Minh Nguyen <ttn64681@uga.edu>
2026-08-20 13:57:51 -0400

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # Find  max left and right bar heights for each i
+        # for each i, min_bar_heights - curr_ith_height 
+        n=len(height)
+        max_l,max_r=[0]*n,[0]*n
+        curr_max_l,curr_max_r=0,0
+        l,r=0,n-1
+        sum=0
+        for i in range(n): # Time O(n)
+            curr_max_l=max(curr_max_l,height[l])
+            max_l[l]=curr_max_l
+            curr_max_r=max(curr_max_r,height[r])
+            max_r[r]=curr_max_r
+            l+=1
+            r-=1
+            # print(f"max_l:{max_l},\nmax_r:{max_r}\n")
+        for i in range(n): # Time O(n)
+            min_h=max(0,min(max_l[i],max_r[i])-height[i])
+            sum+=min_h
+            # print(f"i:{i},l:{l},r:{r},min_h:{min_h}\n")
+        return sum
+        # Overall Space O(n), Time O(n)
+        
+
